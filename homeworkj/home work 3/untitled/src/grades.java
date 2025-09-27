b0a667bd6b093a6356125e0cbcc49a6cf3a07364
muradhuseynoff10-cpu <muradhuseynoff10@gmail.com>
2025-09-27 19:10:29 +0400

    Create grades.java
    grades

---
+++ homeworkj/home work 3/untitled/src/grades.java
@@ -0,0 +1,21 @@
+public class grades {
+    public static void main(String[] args) {
+        int grade = 5;
+
+        switch (grade) {
+            case 5:
+                System.out.println("Отлично");
+                break;
+            case 4:
+                System.out.println("Хорошо");
+                break;
+            case 3:
+                System.out.println("Удовлетворительно");
+                break;
+            case 2:
+                System.out.println("Неудовлетворительно");
+                break;
+            
+        }
+    }
+}
